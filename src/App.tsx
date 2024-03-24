2dd6991d71252014476c531c51d2b3babcc16ddf
Ah-ae <kimaae@gmail.com>
2024-03-24 21:27:41 +0900

    render 100,000 sentences without virtualization

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { faker } from "@faker-js/faker";
+
+const randomNumber = (min: number, max: number) =>
+  faker.datatype.number({ min, max });
+
+const sentences = new Array(100_000)
+  .fill(true)
+  .map(() => faker.lorem.sentence(randomNumber(20, 70)));
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div style={{ width: 400, height: 400, overflowY: "auto" }}>
+      {sentences.map((sentence, index) => (
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          <span>{index + 1}. </span>
+          {sentence}
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
