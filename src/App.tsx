e012ee25142c455c3a5d6272737c2b34281cbe5b
Ah-ae <kimaae@gmail.com>
2024-03-24 21:36:03 +0900

    render 100,000 sentences with virtualization

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef } from "react";
+import { useVirtualizer } from "@tanstack/react-virtual";
+import { faker } from "@faker-js/faker";
+
+const randomNumber = (min: number, max: number) =>
+  faker.datatype.number({ min, max });
+
+const sentences = new Array(100_000)
+  .fill(true)
+  .map(() => faker.lorem.sentence(randomNumber(20, 70)));
 
 function App() {
-  const [count, setCount] = useState(0)
+  const parentElement = useRef(null);
+
+  const virtualizer = useVirtualizer({
+    count: sentences.length, // virtualize할 아이템 전체 개수
+    getScrollElement: () => parentElement.current,
+    estimateSize: () => 45, // estimated size of each item (unit: pixel, largest possible size in case of dynamic height)
+  });
+
+  const items = virtualizer.getVirtualItems(); // viewport에 보여지는 아이템
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div
+      ref={parentElement}
+      style={{ width: 400, height: 400, overflowY: "auto" }}
+    >
+      {/* outer container */}
+      <div
+        style={{
+          position: "relative",
+          height: virtualizer.getTotalSize(),
+          width: "100%",
+        }}
+      >
+        {/* inner container */}
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            transform: `translateY(${items[0]?.start}px)`,
+          }}
+        >
+          {items.map((item) => (
+            <p
+              ref={virtualizer.measureElement}
+              key={item.key}
+              data-index={item.index}
+            >
+              <span>{item.index + 1}. </span>
+              {sentences[item.index]}
+            </p>
+          ))}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
